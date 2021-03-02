424b3a5640e16f937688e77dc83ad8daa0c7e719
Bash-ton <sebbepaz@gmail.com>
2021-03-02 15:00:28 +0100

    clients can now only send one application

--- client/src/Model/Redux/Actions/AuthActions.js
+++ client/src/Model/Redux/Actions/AuthActions.js
@@ -4,8 +4,6 @@
  * @returns {function(*, *): void} a dispatch call to {UserReducer}
  */
 export const signIn = (user) => {
-    console.log(user.data)
-    debugger
     return (dispatch, getState) => {
         dispatch({type:"LOG_IN_USER", role:user.data.role, fname:user.data.firstName, lname: user.data.lastName, dateOfBirth: user.data.dateOfBirth, email: user.data.email})
     }
