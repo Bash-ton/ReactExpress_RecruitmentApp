d6b2c3100d5ecc8cbe1c2154fffc9cc60179f730
Bash-ton <sebbepaz@gmail.com>
2021-03-02 11:57:21 +0100

    cleaned and commented code

--- client/src/Views/Authentication.js
+++ client/src/Views/Authentication.js
@@ -1,15 +1,19 @@
 import React, {useEffect, useState} from 'react'
-import {Field, Form, Formik, useField} from "formik";
+import {Form, Formik, useField} from "formik";
 import {TextField, Button, Select, MenuItem} from "@material-ui/core";
 import * as Yup from "yup"
-import { useDispatch, useSelector } from "react-redux";
-import UserReducer from "../Model/Redux/Reducers/UserReducer";
-import {signIn, signUP, testing} from "../Model/Redux/Actions/testLogInLocalStorage";
-import ApiCall from "../Model/apiCall";
-import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import {signIn} from "../Model/Redux/Actions/testLogInLocalStorage";
+//TODO remove the <Pre-tags> when done testing
+
+/**
+ * This is the authentication page that handles login and signup including validation.
+ * @param apiCall a model document containingn axios calls to the express rest API
+ * @returns {JSX.Element} renders login and signup form
+ */
 const Authentication = ({apiCall}) => {
 
-    //hooks
+    //constants from useState and redux
     const [authStatus, setAuthStatus] = useState("Sign up");
     const [changeAuthStatus, setChangeAuthStatus] = useState("Login");
     const isLoggedIn = useSelector(state => state.UserReducer.userInfo[0].isLoggedIn);
@@ -17,20 +21,20 @@ const Authentication = ({apiCall}) => {
     const dispatch = useDispatch();
 
 
-   useEffect(()=>{
-        if(role === "client"){
+    //life cycle methods
+    useEffect(() => {
+        if (role === "client") {
             window.location = "/user/application";
 
-        }else if(role === "admin"){
+        } else if (role === "admin") {
             window.location = "/admin/applications";
 
         }
     }, [isLoggedIn])
 
 
-// let param = auth/login
 
-    //custom Formik components
+    //custom formik components
     const AuthTextField = ({
                                placeholder,
                                ...props
@@ -47,7 +51,7 @@ const Authentication = ({apiCall}) => {
             />
         )
     }
-    //yup schema
+    //yup schema for validation
     const AuthSchema = Yup.object().shape({
         email: Yup.string()
             .min(4, 'Too Short!')
@@ -104,166 +108,130 @@ const Authentication = ({apiCall}) => {
     return (
         <div>
             {!isLoggedIn
-                ?<div>
-                <Formik
-                    initialValues={{
-                        username: "",
-                        email: "",
-                        password: "",
-                        firstName: "",
-                        lastName: "",
-                        dateOfBirth: [{year: "", month: "", day: ""}]
-                    }}
-                    onSubmit={(data, {setSubmitting, resetForm}) => {
-                        setSubmitting(true);
-                        resetForm();
-                        //TODO uncomment this and use real actions with dispatch (call api from within actions)
-                       /*  switch(authStatus){
-                            case "Login":
-                            dispatch()//to action that handles login
-                            break;
-                            case "Sign up":
-                            dispatch()//to action that handles signup
-                            break;
-                            default:
-                            break;
-                        } */
-                         
-                        let obj = {email: "test@kth.se", password:"test"};
-                        let param = "auth/login";
+                ? <div>
+                    <Formik
+                        initialValues={{
+                            username: "",
+                            email: "",
+                            password: "",
+                            firstName: "",
+                            lastName: "",
+                            dateOfBirth: [{year: "", month: "", day: ""}]
+                        }}
+                        onSubmit={(data, {setSubmitting, resetForm}) => {
+                            setSubmitting(true);
+                            resetForm();
 
-                        if(authStatus ===  "Sign up"){
-                            const instance = apiCall.apiAxios();
+                            if (authStatus === "Sign up") {
+                                const instance = apiCall.apiAxios();
 
-                            instance.post('auth/register', {data})
-                                .then((response) => {
-                                    //dispatch(signUP(response))
-                                    const instance = apiCall.apiAxios();
-                                    instance.post('auth/login', {email: data.email, password: data.password})
-                                        .then((response1) => {
-                                            // console.log(response.headers["set-cookie"]);
-                                            console.log(response1)
-                                            // console.log(document.cookie)
-                                            dispatch(signIn(response1))
-                                          //  dispatch(signUP(data)) //TODO change to signIn
-                                           // instance.get("posts").then(r =>console.log(r) )
-                                        }, (error) => {
-                                            console.log(error);
-                                        });
-                                   // instance.get("posts").then(r =>console.log(r) )
-                                }, (error) => {
-                                    console.log(error);
-                                });
-                        }
-                      else{
-                            const instance = apiCall.apiAxios();
-                            console.log(data);
+                                instance.post('auth/register', {data})
+                                    .then((response) => {
 
-                            instance.post('auth/login', {email: data.email, password: data.password})
-                                .then((response1) => {
-                                    // console.log(response.headers["set-cookie"]);
-                                    console.log(response1)
-                                    // console.log(document.cookie)
-                                    dispatch(signIn(response1))
-                                   // dispatch(testing())//TODO change to signIn
-                                    // instance.get("posts").then(r =>console.log(r) )
-                                }, (error) => {
-                                    console.log(error);
-                                });
+                                        const instance = apiCall.apiAxios();
+                                        instance.post('auth/login', {email: data.email, password: data.password})
+                                            .then((response1) => {
 
+                                                dispatch(signIn(response1))
 
-                            //instance.get("posts").then(r => console.log(r))
+                                            }, (error) => {
+                                                console.log(error);
+                                            });
 
-                        } 
-                    
-                        //;//testing remove this when the TODO above is done
-                        console.log("submit:", {data: data});
-                        setSubmitting(false);
-                    }}
+                                    }, (error) => {
+                                        console.log(error);
+                                    });
+                            } else {
+                                const instance = apiCall.apiAxios();
+                                instance.post('auth/login', {email: data.email, password: data.password})
+                                    .then((response1) => {
 
+                                        dispatch(signIn(response1))
 
-                    
-                    validationSchema={AuthSchema}
+                                    }, (error) => {
+                                        console.log(error);
+                                    });
+                            }
+                            setSubmitting(false);
+                        }}
+                        validationSchema={AuthSchema}
+                    >
+                        {({values, isSubmitting, resetForm, errors}) => (
+                            <Form>
+                                <div>
+                                    <AuthTextField
+                                        placeholder="Email"
+                                        name="email"
+                                        type="input"
+                                    />
+                                </div>
+                                <div>
+                                    <AuthTextField
+                                        placeholder="Password"
+                                        name="password"
+                                        type="password"
+                                    />
+                                </div>
+                                {(authStatus === "Sign up") ? <div>
+                                    <AuthTextField
+                                        placeholder="Username"
+                                        name="username"
+                                        type="input"
+                                    />
+                                </div> : ""}
+                                {(authStatus === "Sign up") ? <div>
+                                    <AuthTextField
+                                        placeholder="First Name"
+                                        name="firstName"
+                                        type="input"
+                                    />
+                                </div> : ""}
+                                {(authStatus === "Sign up") ? <div>
+                                    <AuthTextField
+                                        placeholder="Last Name"
+                                        name="lastName"
+                                        type="input"
+                                    />
+                                </div> : ""}
+                                {(authStatus === "Sign up") ? <div>
+                                    <AuthTextField
+                                        placeholder="Year"
+                                        name="dateOfBirth[0].year"
+                                        type="number"
+                                    />
+                                    <AuthTextField
+                                        placeholder="Month"
+                                        name="dateOfBirth[0].month"
+                                        type="number"
+                                    />
+                                    <AuthTextField
+                                        placeholder="Day"
+                                        name="dateOfBirth[0].day"
+                                        type="number"
+                                    />
+                                </div> : ""}
+                                <div>
+                                    <Button disabled={isSubmitting} type="submit">{authStatus}</Button>
+                                </div>
 
-                >
-                    {({values, isSubmitting, resetForm, errors}) => (
-                        <Form>
-                            <div>
-                                <AuthTextField
-                                    placeholder="Email"
-                                    name="email"
-                                    type="input"
-                                />
-                            </div>
-                            <div>
-                                <AuthTextField
-                                    placeholder="Password"
-                                    name="password"
-                                    type="password"
-                                />
-                            </div>
-                            {(authStatus === "Sign up") ? <div>
-                                <AuthTextField
-                                    placeholder="Username"
-                                    name="username"
-                                    type="input"
-                                />
-                            </div> : ""}
-                            {(authStatus === "Sign up") ? <div>
-                                <AuthTextField
-                                    placeholder="First Name"
-                                    name="firstName"
-                                    type="input"
-                                />
-                            </div> : ""}
-                            {(authStatus === "Sign up") ? <div>
-                                <AuthTextField
-                                    placeholder="Last Name"
-                                    name="lastName"
-                                    type="input"
-                                />
-                            </div> : ""}
-                            {(authStatus === "Sign up") ? <div>
-                                <AuthTextField
-                                    placeholder="Year"
-                                    name="dateOfBirth[0].year"
-                                    type="number"
-                                />
-                                <AuthTextField
-                                    placeholder="Month"
-                                    name="dateOfBirth[0].month"
-                                    type="number"
-                                />
-                                <AuthTextField
-                                    placeholder="Day"
-                                    name="dateOfBirth[0].day"
-                                    type="number"
-                                />
-                            </div> : ""}
-                            <div>
-                                <Button disabled={isSubmitting} type="submit">{authStatus}</Button>
-                            </div>
-
-                            <pre>{JSON.stringify(values, null, 2)}</pre>
-                            <pre>{JSON.stringify(errors, null, 2)}</pre>
+                                <pre>{JSON.stringify(values, null, 2)}</pre>
+                                <pre>{JSON.stringify(errors, null, 2)}</pre>
 
 
-                            <div onClick={() => {
-                                handleAuthStatusChange();
-                                resetForm()
-                            }}>{changeAuthStatus}</div>
-                        </Form>
-                    )}
+                                <div onClick={() => {
+                                    handleAuthStatusChange();
+                                    resetForm()
+                                }}>{changeAuthStatus}</div>
+                            </Form>
+                        )}
+                    </Formik>
+                    <div onClick={() => {
+                        changeView("/admin/signup")
+                    }}>sign up new admin
+                    </div>
 
-
-                </Formik>
-                <div onClick={() => {
-                    changeView("/admin/signup")
-                }}>sign up new admin
                 </div>
-
-            </div>
-            :""}
+                : ""}
         </div>
     )
 }
