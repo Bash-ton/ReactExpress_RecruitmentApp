6537b4c0e08626e30d4e5c99025e56ac64e6f85a
Bash-ton <sebbepaz@gmail.com>
2021-03-08 14:34:45 +0100

    merged main

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import {Component} from "react";
 import './App.css';
-import { Route, Switch, Link } from "react-router-dom"
+import {Route, Switch, Link} from "react-router-dom"
 
 //models
 import ApiCall from './Model/apiCall';
@@ -13,18 +13,21 @@ import Authentication from "./Views/Authentication";
 import SignUpAdmin from "./Views/SignUpAdmin";
 import AdminApplications from "./Views/AdminApplications";
 import Header from "./Views/Header";
-<<<<<<< HEAD
-import MigrateFromOldDB from "./Views/MigrateFromOldDB";
-=======
-import { Page404 } from "./Views/ErrorPages";
+import {Page404} from "./Views/ErrorPages";
 
->>>>>>> main
+
+import MigrateFromOldDB from "./Views/MigrateFromOldDB";
+
+
+
 /**
  * Main file for the frontend
  * Handles initial import of the model and routing of the frontend app
  */
-class App extends Component {
-    constructor(props) {
+class App extends Component
+{
+    constructor(props)
+    {
         super(props);
         this.state = {
             apiCall: new ApiCall(),
@@ -32,41 +35,42 @@ class App extends Component {
         }
     }
 
-    render() {
+    render()
+    {
         return (
             <div className="applicationApp">
                 <header className="recruitmentApp">
                     <Header/>
                 </header>
                 <Switch>
-                <Route
-                    exact path="/"
-                    render={() => <Authentication apiCall={this.state.apiCall}/>}
-                />
-                <Route
-                    exact path="/admin/signup"
-                    render={() => <SignUpAdmin/>}
-                />
-                <Route
-                    exact path="/user/application"
-                    render={() => <Expertise model={this.state.model} apiCall={this.state.apiCall}/>}
-                />
-                <Route
-                    exact path="/admin/applications"
-                    render={() => <AdminApplications apiCall={this.state.apiCall}/>}
-                />
-                <Route
-<<<<<<< HEAD
-                    exact path="/admin/Migrate"
-                    render={() => <MigrateFromOldDB apiCall={this.state.apiCall}/>}
-                />
+                    <Route
+                        exact path="/"
+                        render={() => <Authentication apiCall={this.state.apiCall}/>}
+                    />
+                    <Route
+                        exact path="/admin/signup"
+                        render={() => <SignUpAdmin/>}
+                    />
+                    <Route
+                        exact path="/user/application"
+                        render={() => <Expertise model={this.state.model} apiCall={this.state.apiCall}/>}
+                    />
+                    <Route
+                        exact path="/admin/applications"
+                        render={() => <AdminApplications apiCall={this.state.apiCall}/>}
+                    />
+                    <Route
 
-=======
-                    exact path="*"
-                    render={() =><Page404/>}
-                />
+                        exact path="/admin/Migrate"
+                        render={() => <MigrateFromOldDB apiCall={this.state.apiCall}/>}
+                    />
+
+                    <Route
+                        exact path="*"
+                        render={() => <Page404/>}
+                    />
                 </Switch>
->>>>>>> main
+
             </div>
         );
 
